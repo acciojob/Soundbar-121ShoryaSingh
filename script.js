b0a73ad412ea2da3b69368c88fe34594487b9f00
Shorya <139441120+121ShoryaSingh@users.noreply.github.com>
2025-10-11 16:46:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+
+const sounds = ["rain", "ocean", "bird", "wind", "fire"];
+
+const buttonContainer = document.getElementById("buttons");
+
+sounds.forEach((sound) => {
+	const btn = document.createElement("button");
+	btn.classList.add("btn");
+	btn.innerText =sound;
+
+	btn.addEventListener("click", () => {
+		stopAllSound();
+		const audio = new Audio(`sound/${sound}.mp3`);
+		audio.play();
+		btn.currentAudio = audio;
+	})
+
+	buttonContainer.appendChild(btn);
+})
+
+const stopBtn = document.createElement("button");
+stopBtn.classList.add("stop");
+stopBtn.innerText = "stop";
+
+stopBtn.addEventListener('click', stopAllSounds);
+buttonContainer.appendChild(stopBtn);
+
+function stopAllSound() {
+	const audios = document.querySelector('audio');
+	audios.forEach(a => a.pause());
+}
